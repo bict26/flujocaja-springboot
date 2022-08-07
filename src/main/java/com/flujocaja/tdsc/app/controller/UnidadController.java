324fc700d20d8d35918a4cf0ee7b5ee0b850be36
borisisaacx <borisisaac63@gmail.com>
2022-08-07 01:26:03 -0500

    se agrega el metodo para agregar unidad

--- src/main/java/com/flujocaja/tdsc/app/controller/UnidadController.java
+++ src/main/java/com/flujocaja/tdsc/app/controller/UnidadController.java
@@ -3,9 +3,12 @@ package com.flujocaja.tdsc.app.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,12 @@ public class UnidadController {
 	
 	@Autowired
 	private UnidadService unidadService;
+	
+	@PostMapping
+	public ResponseEntity<?> create (@RequestBody Unidad unidad){
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(unidadService.save(unidad));
+	}
 	
 	
 	@GetMapping("/{id}")
