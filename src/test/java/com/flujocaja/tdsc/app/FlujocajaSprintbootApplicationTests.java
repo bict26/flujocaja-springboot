276b33ae241aca973dca542579d7e6b97c2649d5
borisisaacx <borisisaac63@gmail.com>
2022-08-06 23:46:08 -0500

    init project

---
+++ src/test/java/com/flujocaja/tdsc/app/FlujocajaSprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flujocaja.tdsc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlujocajaSprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
